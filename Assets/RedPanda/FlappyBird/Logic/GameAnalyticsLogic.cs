2bdf681c02377e389fee1a041900cf167ed4ce28
Admor Aguilar <admor.aguilar@gmail.com>
2022-10-08 19:34:10 +0800

    + Integrate GameAnalytics SDK

---
+++ Assets/RedPanda/FlappyBird/Logic/GameAnalyticsLogic.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using WaterToolkit.Data;
+
+namespace FlappyBird
+{
+	public class GameAnalyticsLogic : MonoBehaviour
+	{
+		[SerializeField]
+		private bool _isEnableAnalytics = false;
+
+		private GameAnalyticsWrapper _gameAnalyticsWrapper = null;
+		private FlowDirectoryData _flowDirectoryData = null;
+		
+		private void OnFlowInitializeVisitMethod()
+		{
+			_gameAnalyticsWrapper.Initialize();
+		}
+
+		private void OnFlowInGameVisitMethod()
+		{
+			_gameAnalyticsWrapper.GameStart();
+		}
+
+		private void OnFlowGameOverVisitMethod()
+		{
+			_gameAnalyticsWrapper.GameFinish();
+		}
+
+		private void Awake()
+		{
+			_gameAnalyticsWrapper = Blackboard.Get<GameAnalyticsWrapper>();
+			_flowDirectoryData = Blackboard.Get<FlowDirectoryData>();
+
+			if(!_isEnableAnalytics || Application.isEditor) {
+				Destroy(_gameAnalyticsWrapper.gameObject);
+				Destroy(gameObject);
+				return;
+			}
+		}
+
+		private void OnEnable()
+		{
+			_flowDirectoryData.initialize.OnVisit += OnFlowInitializeVisitMethod;
+			_flowDirectoryData.inGame.OnVisit += OnFlowInGameVisitMethod;
+			_flowDirectoryData.gameOver.OnVisit += OnFlowGameOverVisitMethod;
+		}
+
+		private void OnDisable()
+		{
+			_flowDirectoryData.initialize.OnVisit -= OnFlowInitializeVisitMethod;
+			_flowDirectoryData.inGame.OnVisit -= OnFlowInGameVisitMethod;
+			_flowDirectoryData.gameOver.OnVisit -= OnFlowGameOverVisitMethod;
+		}
+	}
+}
---
+++ Assets/RedPanda/FlappyBird/View/GameAnalytics/GameAnalyticsWrapper.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using GameAnalyticsSDK;
+using WaterToolkit.Data;
+
+namespace FlappyBird
+{
+	[RequireComponent(typeof(GameAnalytics))]
+	public class GameAnalyticsWrapper : MonoBehaviour, IGameAnalyticsATTListener
+	{
+		[SerializeField]
+		private GameAnalytics _sdkInstance = null;
+
+		public GameAnalytics sdkInstance => _sdkInstance;
+
+		public void Initialize()
+		{
+			if(Application.platform == RuntimePlatform.IPhonePlayer) {
+				GameAnalytics.RequestTrackingAuthorization(this);
+			} else {
+				GameAnalytics.Initialize();
+			}
+		}
+
+		public void GameStart()
+		{
+			GameAnalytics.NewProgressionEvent(GAProgressionStatus.Start, "default_level");
+		}
+
+		public void GameFinish()
+		{
+			GameAnalytics.NewProgressionEvent(GAProgressionStatus.Complete, "default_level");
+		}
+
+		public void SubmitScore(int score)
+		{
+			GameAnalytics.NewDesignEvent("game:score", score);
+		}
+
+		void IGameAnalyticsATTListener.GameAnalyticsATTListenerNotDetermined() => GameAnalytics.Initialize();
+
+		void IGameAnalyticsATTListener.GameAnalyticsATTListenerRestricted() => GameAnalytics.Initialize();
+
+		void IGameAnalyticsATTListener.GameAnalyticsATTListenerDenied() => GameAnalytics.Initialize();
+
+		void IGameAnalyticsATTListener.GameAnalyticsATTListenerAuthorized() => GameAnalytics.Initialize();
+	}
+}
